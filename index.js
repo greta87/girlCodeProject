c7cb185cb9b12e5a531279950e636efd4b7cd591
Greta Gotlieb <gretagotlieb@gmail.com>
2017-11-14 19:01:31 +1300

    All files

---
+++ index.js
@@ -0,0 +1,35 @@
+//set up
+var express = require('express')
+var app = express();
+var bodyParser = require('body-parser')
+
+//If a client asks for a file,
+//look in the public folder. If it's there, give it to them.
+app.use(express.static(__dirname + '/public'));
+
+//this lets us read POST data
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+//make an empty list
+var posts = [];
+
+//let a client GET the list
+var sendPostsList = function (request, response) {
+  response.send(posts);
+}
+app.get('/posts', sendPostsList);
+
+//let a client POST something new
+var saveNewPost = function (request, response) {
+  console.log(request.body.message); //write it on the command prompt so we can see
+  var post= {};
+  post.message = request.body.message;
+  posts.push(post); //save it in our list
+  response.send("thanks for your message. Press back to add another");
+}
+app.post('/posts', saveNewPost);
+
+//listen for connections on port 3000
+app.listen(3000);
+console.log("Hi! I am listening at http://localhost:3000");
